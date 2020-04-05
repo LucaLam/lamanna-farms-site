e0ce1c5d50200ad6c80f6625e50de62faa8eba73
Luca Lamanna <luca.lamanna@hotmail.com>
2020-04-04 21:38:41 -0400

    header complete - need to add links later on using react router link or smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 
+import Header from "./components/header/header";
+
 
 function App() {
   return (
     <div className="container">
-      HELLO WORLD!
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import './header.scss';
+import logo from "../../assets/logo.png";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="header__logo-container">
+                    <img className="header__logo" src={logo} alt="Lamanna Farms"/>
+                </div>
+                <ul className="nav__list">
+                    <li className="nav__item">About</li>
+                    <li className="nav__item">Shop</li>
+                    <li className="nav__item">Contact Us</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Header;
