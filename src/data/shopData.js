b6ecff80405e6a0223cf86b271eaeb669ead8b8e
Luca Lamanna <luca.lamanna@hotmail.com>
2020-04-06 12:44:34 -0400

    data updated

--- src/data/shopData.js
+++ src/data/shopData.js
@@ -38,6 +38,147 @@ const shopData = [
         description: "Farm grown organic peppers under organic Italian extra virgin olive oil.",
         cost: 5,
         img: ""
+    },
+
+    {
+        id: 6,
+        name: "Banana & Dark Chocolate Chip Loaf",
+        description: "Delicious banana loaf loaded with dark chocolate chips.",
+        cost: 8,
+        img: ""
+    },
+
+    {
+        id: 7,
+        name: "Zucchini Chocolate Cake",
+        description: "Farm grown zucchini, shredded and turned into a delicious chocolate cake using organic Ecuadorian cocoa and dark chocolate chips.",
+        cost: 8,
+        img: ""
+    },
+
+    {
+        id: 8,
+        name: "Tiramisu (12pcs)",
+        description: "A great pick-me-up, made with....",
+        cost: 45,
+        img: ""
+    },
+
+    {
+        id: 9,
+        name: "Fresh Tomato & Basil Sauce (1L)",
+        description: "Organic farm grown tomatoes and basil mixed into a classic Italian red sauce. Note, this sauce needs to be cooked.",
+        cost: 10,
+        img: ""
+    },
+
+    {
+        id: 10,
+        name: "Ready-made Tomato sauce with basil, onion, and garlic (1L)",
+        description: "Farm grown tomatoes, basil, onion, and garlic mixed into a delicious sauce.",
+        cost: 13,
+        img: ""
+    },
+
+    {
+        id: 11,
+        name: "Ready-made Pizza Sauce (1L)",
+        description: "Farm grown tomatoes, garlic, and wild Mediterranean oregano perfect for pizza.",
+        cost: 12,
+        img: ""
+    },
+    {
+        id: 12,
+        name: "Chicken Broth (1L)",
+        description: "A heartwarming broth made from local, organic chickens.",
+        cost: 12,
+        img: ""
+    },
+
+    {
+        id: 13,
+        name: "Beef Broth (1L)",
+        description: "A heartwarming broth made from local, organic beef bones.",
+        cost: 13,
+        img: ""
+    },
+    {
+        id: 15,
+        name: "Tagliatlle (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 5,
+        img: ""
+    },
+    {
+        id: 16,
+        name: "Pappardelle (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 5,
+        img: ""
+    },
+    {
+        id: 17,
+        name: "Spaghetti (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 5,
+        img: ""
+    },
+    {
+        id: 18,
+        name: "Lasagna Sheets (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 5,
+        img: ""
+    },
+    {
+        id: 19,
+        name: "Garganelli (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 7,
+        img: ""
+    },
+    {
+        id: 20,
+        name: "Gnocchietti (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 7,
+        img: ""
+    },
+    {
+        id: 21,
+        name: "Trofie (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 7,
+        img: ""
+    },
+    {
+        id: 22,
+        name: "Tortellini - Traditional (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 9,
+        img: ""
+    },
+    {
+        id: 23,
+        name: "Tortellini - Cheese (120g)",
+        description: "Handmade with locally sourced flour and farm fresh eggs.",
+        cost: 9,
+        img: ""
+    },
+
+    {
+        id: 24,
+        name: "Potato Gnocchi(120g)",
+        description: "Handmade with Canadian potatoes and farm fresh eggs.",
+        cost: 5,
+        img: ""
+    },
+    {
+        id: 25,
+        name: "Ready-made Lasagna",
+        description: "6-layers of heaven. Made with organic, farm grown products, handmade pasta sheets . Note, Small (6-8 servings) is $75, while Large (10-12 servings) is $110)",
+        cost: 9,
+        img: ""
     }
 
 ]
